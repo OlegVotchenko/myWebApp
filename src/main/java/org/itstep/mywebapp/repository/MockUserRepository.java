67ff924e9f011bc2ebe367bdb697a9d5b0e56d17
Oleg Vothenko <votolegforreg@gmail.com>
2017-06-26 22:03:55 +0300

    11 add logging to repository

--- src/main/java/org/itstep/mywebapp/repository/MockUserRepository.java
+++ src/main/java/org/itstep/mywebapp/repository/MockUserRepository.java
@@ -1,40 +1,55 @@
 package org.itstep.mywebapp.repository;
 
 import org.itstep.mywebapp.model.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class MockUserRepository implements UserRepository {
 
+    private static final Logger LOG = LoggerFactory.getLogger(MockUserRepository.class);
+
     private Map<Integer, User> users = new ConcurrentHashMap<>();
 
+    private AtomicInteger idCounter = new AtomicInteger(1);
+
     {
-        users.put(1, new User(1,"Mike", LocalDateTime.now()));
-        users.put(2, new User(2, "Natasha", LocalDateTime.now()));
+        save(new User("Mike"));
+        save(new User("Natasha"));
     }
 
     public List<User> getAll() {
+        LOG.debug("Get all users");
         return new ArrayList<>(users.values());
     }
 
     @Override
     public void delete(Integer id) {
+        LOG.debug("Delete user with id = {}", id);
         users.remove(id);
     }
 
     @Override
     public User get(Integer id) {
+        LOG.debug("Get user with id = {}", id);
         return users.get(id);
     }
 
     @Override
-    public void save(User user) {
+    public User save(User user) {
+        if (user.getId() == null) {
+            user.setId(idCounter.getAndIncrement());
+            user.setRegistered(LocalDateTime.now());
+        }
+        LOG.debug("Save user with id = {}", user.getId());
         users.put(user.getId(), user);
+        return user;
     }
-
 
 }
